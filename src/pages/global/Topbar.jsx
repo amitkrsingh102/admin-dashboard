5904cba8cb85a6854b11139fea74d437b9193000
AmitSingh <amitkrsingh102@gmail.com>
2023-03-09 06:49:56 +0530

    sidebar done

--- src/pages/global/Topbar.jsx
+++ src/pages/global/Topbar.jsx
@@ -12,16 +12,38 @@ const Topbar = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
   const colorMode = useContext(ColorModeContext);
+
   return (
-    <Box display={"flex"} justifyContent={"space-between"} p={2}>
+    <Box display="flex" justifyContent="space-between" p={2}>
+      {/* search section */}
       <Box
-        display={"flex"}
-        background={colors.primary[400]}
-        border={"1px solid black"}
-        borderRadius={"3px"}
+        display="flex"
+        backgroundColor={colors.primary[400]}
+        borderRadius="3px"
       >
         <InputBase sx={{ ml: 2, flex: 1 }} placeholder={"Search"} />
-        <SearchIcon />
+        <IconButton type="button" sx={{ p: 1 }}>
+          <SearchIcon />
+        </IconButton>
+      </Box>
+      {/* icon section */}
+      <Box display="flex">
+        <IconButton onClick={colorMode.toggleColorMode}>
+          {theme.palette.mode === "dark" ? (
+            <LightModeOutlinedIcon />
+          ) : (
+            <DarkModeOutlinedIcon />
+          )}
+        </IconButton>
+        <IconButton>
+          <NotificationsOutlinedIcon />
+        </IconButton>
+        <IconButton>
+          <SettingsOutlinedIcon />
+        </IconButton>
+        <IconButton>
+          <PersonOutlinedIcon />
+        </IconButton>
       </Box>
     </Box>
   );
